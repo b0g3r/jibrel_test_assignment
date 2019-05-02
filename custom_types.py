851b686ac8ab3339386518cd440e585c232233a3
Dima Boger <kotvberloge@gmail.com>
2019-05-02 12:25:38 +0300

    Add throw mode

--- custom_types.py
+++ custom_types.py
@@ -8,4 +8,6 @@ FINISH_REQUEST = 'FinishRequest'
 BACKEND_CONNECT = 'BackendConnect'
 BACKEND_OK = 'BackendOk'
 
+REQUIRED_TYPES = {START_REQUEST, FINISH_REQUEST, BACKEND_OK, BACKEND_CONNECT}
+
 EventType = Tuple[int, int, str, Optional[int], Optional[str]]
--- log_parser.py
+++ log_parser.py
@@ -1,12 +1,12 @@
 """
 Contains a few util functions and generators for parsing logs and converting to basic python types.
 """
-from typing import IO, Iterator
+from typing import IO, Iterator, Optional
 
-from custom_types import EventType
+from custom_types import EventType, REQUIRED_TYPES
 
 
-def log_file(file_object: IO[str]) -> Iterator[EventType]:
+def log_file(file_object: IO[str], throw: bool) -> Iterator[EventType]:
     """
     Generator for line-by-line log reading.
 
@@ -15,10 +15,12 @@ def log_file(file_object: IO[str]) -> Iterator[EventType]:
     and chunk-reading isn't necessary.
     """
     for line in file_object:
-        yield convert_line(line.strip())
+        event = convert_line(line.strip(), throw=throw)
+        if event is not None:  # filter for throw mode
+            yield event
 
 
-def convert_line(log_line: str) -> EventType:
+def convert_line(log_line: str, throw: bool) -> Optional[EventType]:
     """
     Convert log line to native python types.
 
@@ -27,7 +29,11 @@ def convert_line(log_line: str) -> EventType:
     backend_id = None
     payload = None
 
-    timestamp_raw, request_id_raw, request_type_raw, *optional_payload = log_line.split('\t')
+    timestamp_raw, request_id_raw, event_type, *optional_payload = log_line.split('\t')
+
+    # if throw mode is on we just throw events with not interested type
+    if throw and event_type not in REQUIRED_TYPES:
+        return None
 
     # little hack for DRY converting
     if len(optional_payload) > 0:
@@ -35,4 +41,4 @@ def convert_line(log_line: str) -> EventType:
     if len(optional_payload) > 1:
         payload = optional_payload[1]
 
-    return int(timestamp_raw), int(request_id_raw), request_type_raw, backend_id, payload
+    return int(timestamp_raw), int(request_id_raw), event_type, backend_id, payload
